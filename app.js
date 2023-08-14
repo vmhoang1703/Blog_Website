73f4ac5be7734bf43f6df7903fa563df69d77ab7
vmhoang1703 <vmhoang1703@gmail.com>
2023-08-14 20:36:44 +0700

    complete Blog Website following Udemy Courses

---
+++ app.js
@@ -0,0 +1,102 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require('lodash');
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+let postArray = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", async(req, res) => {
+  try {
+    res.render("home.ejs", {
+      startingContent: homeStartingContent,
+      posts: postArray,
+    });
+  } catch (error) {
+    res.status(404).send("<h1>Not Found</h1>");
+  }
+})
+
+
+app.get("/about", async(req, res) => {
+  try {
+    res.render("about.ejs", {
+      aboutContent: aboutContent,
+    });
+  } catch (error) {
+    res.status(404).send("<h1>Not Found</h1>");
+  }
+})
+
+app.get("/contact", async(req, res) => {
+  try {
+    res.render("contact.ejs", {
+      contactContent: contactContent,
+    });
+  } catch (error) {
+    res.status(404).send("<h1>Not Found</h1>");
+  }
+})
+
+app.get("/compose", async(req, res) => {
+  try {
+    res.render("compose.ejs");
+  } catch (error) {
+    res.status(404).send("<h1>Not Found</h1>");
+  }
+})
+
+app.post("/compose", async(req, res) => {
+  try {
+    const post = {
+      postTitle : req.body.title,
+      postBody : req.body.post,
+    }
+    postArray.push(post);
+    res.redirect("/")
+  } catch (error) {
+    res.status(404).send("<h1>Not Found</h1>");
+  }
+})
+
+app.get("/posts/:namePost", async(req, res) => {
+  try {
+    const requestedTitle = _.lowerCase(req.params.namePost);
+    postArray.forEach((post) => {
+      const storedTitle = _.lowerCase(post.postTitle);
+      if(requestedTitle === storedTitle){
+        res.render("post.ejs", {
+          title: post.postTitle,
+          body: post.postBody,
+        });
+      };
+    });
+  } catch (error) {
+    res.status(404).send("<h1>Not Found</h1>");
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
